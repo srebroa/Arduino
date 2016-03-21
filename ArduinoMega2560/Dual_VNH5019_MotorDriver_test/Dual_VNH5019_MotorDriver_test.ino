741458656b69fcdf100e92aae8d05898d41f6a96
srebroa <srebroa@gmail.com>
2016-03-22 00:10:05 +0100

    Dual VNH5019 Arduino Motor Driver Shield Test

---
+++ ArduinoMega2560/Dual_VNH5019_MotorDriver_test/Dual_VNH5019_MotorDriver_test.ino
@@ -0,0 +1,91 @@
+#include "DualVNH5019MotorShield.h"
+#define LOOP_MAX 400
+DualVNH5019MotorShield md;
+
+void stopIfFault(){
+  if (md.getM1Fault())
+  {
+    Serial.println("M1 fault");
+    while(1);
+  }
+  if (md.getM2Fault())
+  {
+    Serial.println("M2 fault");
+    while(1);
+  }
+}
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("Dual VNH5019 Motor Driver Shield Test");
+  md.init();
+}
+
+void loop()
+{
+  for (int i = 0; i <= LOOP_MAX; i++){
+    md.setM1Speed(i);
+    stopIfFault();
+    if (i%200 == 100)
+    {
+      Serial.print("M1 current: ");
+      Serial.println(md.getM1CurrentMilliamps());
+    }
+    delay(2);
+  }
+  
+  for (int i = LOOP_MAX; i >= -LOOP_MAX; i--){
+    md.setM1Speed(i);
+    stopIfFault();
+    if (i%200 == 100)
+    {
+      Serial.print("M1 current: ");
+      Serial.println(md.getM1CurrentMilliamps());
+    }
+    delay(2);
+  }
+  
+  for (int i = -LOOP_MAX; i <= 0; i++){
+    md.setM1Speed(i);
+    stopIfFault();
+    if (i%200 == 100)
+    {
+      Serial.print("M1 current: ");
+      Serial.println(md.getM1CurrentMilliamps());
+    }
+    delay(2);
+  }
+
+  for (int i = 0; i <= LOOP_MAX; i++){
+    md.setM2Speed(i);
+    stopIfFault();
+    if (i%200 == 100)
+    {
+      Serial.print("M2 current: ");
+      Serial.println(md.getM2CurrentMilliamps());
+    }
+    delay(2);
+  }
+  
+  for (int i = LOOP_MAX; i >= -LOOP_MAX; i--){
+    md.setM2Speed(i);
+    stopIfFault();
+    if (i%200 == 100)
+    {
+      Serial.print("M2 current: ");
+      Serial.println(md.getM2CurrentMilliamps());
+    }
+    delay(2);
+  }
+  
+  for (int i = -LOOP_MAX; i <= 0; i++){
+    md.setM2Speed(i);
+    stopIfFault();
+    if (i%200 == 100)
+    {
+      Serial.print("M2 current: ");
+      Serial.println(md.getM2CurrentMilliamps());
+    }
+    delay(2);
+  }
+}
