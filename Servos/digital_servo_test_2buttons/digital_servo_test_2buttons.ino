0caf0bea85922f960f96f41701edf4c955636306
srebroa <srebroa@gmail.com>
2016-05-14 16:46:46 +0200

    Testing Digital Servo LF-20MG

--- Servos/digital_servo_test_2buttons/digital_servo_test_2buttons.ino
+++ Servos/digital_servo_test_2buttons/digital_servo_test_2buttons.ino
@@ -1,8 +1,11 @@
+// Testing Digital Servo LF-20MG
 #include <Servo.h>
+#define INCREASE_STEP 5
+#define DECREASE_STEP 2
 Servo servo1;
 
-int right = 7; // button Right
-int left = 8;  // button Left
+int right = 7; // Right button
+int left = 8;  // Left button
 int right_state;
 int left_state;
 int servo_val;
@@ -13,25 +16,22 @@ void setup(){
   pinMode(left, INPUT);
   servo1.attach(6);
   servo_val = servo1.read();
-}
+}// void setup()
 
 void loop(){
   right_state = digitalRead(right);
   left_state = digitalRead(left);
  
-  if(right_state == HIGH)
-  {
-    servo1.write(servo_val - 1);
+  if(right_state == HIGH && servo_val>10+DECREASE_STEP){
+    servo1.write(servo_val - DECREASE_STEP);
     delay(14);
     servo_val = servo1.read();
-    Serial.println("Turn Right");
-  }
- 
-  if(left_state == HIGH)
-  {
-  servo1.write(servo_val + 1);
+    Serial.println("Angle: "+String(servo_val));
+  } 
+  if(left_state == HIGH && servo_val<=180-INCREASE_STEP){
+  servo1.write(servo_val + INCREASE_STEP);
   delay(14);
   servo_val = servo1.read();
-  Serial.println("Turn Left");
+  Serial.println("Angle: "+String(servo_val));
   }
-}
+}// void loop()
