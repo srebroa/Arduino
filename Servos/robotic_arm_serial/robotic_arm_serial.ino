47ec68a34897987b660831e087485e11cbb6b5d5
srebroa <srebroa@gmail.com>
2016-05-16 16:44:23 +0200

    Testing Robotic Arm controlled via serial port

---
+++ Servos/robotic_arm_serial/robotic_arm_serial.ino
@@ -0,0 +1,112 @@
+/* Testing Robotic Arm (2 Digital Servos LF-20MG and 1 analog HD-1501MG) controlled via Serial Port
+Examples:
+a45     // base rotation - analog servo
+b90
+c45
+a45b45c45
+*/
+#include <Servo.h>
+#define SERVO1_MIN 10
+#define SERVO1_MAX 180
+#define SERVO2_MIN 10
+#define SERVO2_MAX 180
+#define SERVO3_MIN 10
+#define SERVO3_MAX 180
+
+#define SERVO1_PWM_PIN 3
+#define SERVO2_PWM_PIN 5
+#define SERVO3_PWM_PIN 6
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+
+int servo1_val;
+int servo2_val;
+int servo3_val;
+
+void setup(){
+  Serial.begin(9600);
+  servo1.attach(SERVO1_PWM_PIN);
+  servo2.attach(SERVO2_PWM_PIN);
+  servo3.attach(SERVO3_PWM_PIN);
+  servo1_val = servo1.read();
+  servo2_val = servo2.read();
+  servo3_val = servo3.read();
+}// void setup()
+
+void loop(){
+  while (Serial.available()) {
+    processInput();
+  } 
+}// void loop()
+
+void processInput (){
+  byte c = Serial.read ();
+  switch (c){   
+    case 'a':
+    {
+      int deg = Serial.parseInt();
+      setServo(1, deg);
+      break;
+    }
+
+    case 'b':
+    {
+      int deg = Serial.parseInt();
+      setServo(2, deg);
+      break;
+    }
+    
+    case 'c':
+    {
+      int deg = Serial.parseInt();
+      setServo(3, deg);
+      break;
+    }
+    case 'p':
+    {
+      // Set Default Position
+      setDefaultPosition(90, 120, 120);
+      break;
+    }
+
+    case 'i':
+    {
+      // Print current servos position
+      printServosPosition();
+      break;
+    }
+  }// switch (c)
+}
+
+void printServosPosition(){
+   Serial.println("servo1: "+String(servo1_val));
+   Serial.println("servo2: "+String(servo2_val));
+   Serial.println("servo3: "+String(servo3_val));
+}
+
+void setDefaultPosition(int s1_deg, int s2_deg, int s3_deg){
+    setServo(1, s1_deg);
+    setServo(2, s2_deg);
+    setServo(3, s3_deg);
+}
+
+void setServo(int servoNumber, int deg){
+  if(servoNumber==1 && deg>=SERVO1_MIN && deg<=SERVO1_MAX){
+    servo1.write(deg);
+    servo1_val = servo1.read();
+    Serial.println("servo1: "+String(servo1_val));
+  }
+  else if(servoNumber==2 && deg>=SERVO2_MIN && deg<=SERVO2_MAX){
+    servo2.write(deg);
+    servo2_val = servo2.read();
+    Serial.println("servo2: "+String(servo2_val));
+  }
+  else if(servoNumber==3 && deg>=SERVO3_MIN && deg<=SERVO3_MAX){
+    servo3.write(deg);
+    servo3_val = servo3.read();
+    Serial.println("servo3: "+String(servo3_val));
+  }
+}// void setServo(int servoNumber, int deg)
+
