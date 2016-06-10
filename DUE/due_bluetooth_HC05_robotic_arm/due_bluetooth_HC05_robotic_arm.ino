cca30102bae4e10c44c1d41ca5247ae5258348d9
srebroa <srebroa@gmail.com>
2016-06-10 19:09:04 +0200

    remote-controlled robotic arm update

--- DUE/due_bluetooth_HC05_robotic_arm/due_bluetooth_HC05_robotic_arm.ino
+++ DUE/due_bluetooth_HC05_robotic_arm/due_bluetooth_HC05_robotic_arm.ino
@@ -1,19 +1,23 @@
-/*
-Arduino Due + HC-05 bluetooth + servos + oled
+ /*
+Arduino Due + HC-05 bluetooth + servos + oled + IR sensors
 - servos controlled via bluetooth
 
-Serial (Tx/Rx) communicate to PC via USB
-Serial1 (Tx1/Rx1) connect to HC-05
-HC-05 Rx - Due Tx1 (18)
-HC-05 Tx - Due Rx1 (19)
-HC-05 GND - Due GND
-HC-05 VCC - Due 3.3V
+DUE -> HC-05
+Tx1 (18) - Rx
+Rx1 (19) - Tx
+GND - GND
+3.3V - VCC
 
-// OLED 128x64 -> DUE
-Vcc - 3.3V
-Gnd - Gnd
-Scl - Scl (21)
-Sda - Sda (20)
+DUE -> OLED 128x64
+3.3V - VCC
+GND - GND
+Scl (21) - Scl
+Sda (20) - Sda
+
+DUE -> Analog Distance Sensors (GP2Y0A21YK):
+A0 -> Sensor Right
+A1 -> Sensor Front
+A2 -> Sensor Left
 
 Examples:
 a45     // base rotation - analog servo
@@ -51,6 +55,9 @@ g255o // open full speed
 #define GRIPPER_MOTOR_AIN1 12 
 #define GRIPPER_MOTOR_AIN2 13
 
+//Control State
+#define SEMI_AUTONOMOUS 1
+
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
@@ -66,18 +73,56 @@ int servoBase_val;
 int servoShoulder_val;
 int servoElbow_val;
 
+// Analog IR sensors distance variables
+int distanceSensorRight; 
+int distanceSensorFront; 
+int distanceSensorLeft;
+// Analog IR sensors Pins
+int distanceSensorRightPin = 0;
+int distanceSensorFrontPin = 1;
+int distanceSensorLeftPin = 2;
+int detectionThreshold;
+
+int controlState = 0;
+int baseServoSpeedFlag = 1;
+
+
 void setup(){
-  pinMode(LED, OUTPUT);
+  //pinMode(LED, OUTPUT);
   setOledDisplay();
-  flashLed(300);
+  //flashLed(300);
   Serial.begin(9600);
+  initializeBluetooth();
+  initializeServos();
+  initializeDistanceSensors();
+  delay(2000);
+  setDefaulRoboticArmPosition();
+}
+
+void initializeBluetooth(){
   HC05.begin(9600);
   HC05.setTimeout(10);
-  Serial.write("\nServo test\n");
-  
+}
+
+void initializeServos(){
+  Serial.write("\nServo test\n"); 
   servoBase.attach(BASE_PWM_PIN);
   servoShoulder.attach(SHOULDER_PWM_PIN);
   servoElbow.attach(ELBOW_PWM_PIN);
+}
+
+void initializeDistanceSensors(){
+  detectionThreshold = 440; // can be changed as needed ;)
+  distanceSensorRight = 0;
+  distanceSensorFront = 0;
+  distanceSensorLeft = 0; 
+}
+
+void setDefaulRoboticArmPosition(){
+  setServoBasePosition(90);
+  setServo(2, 90);
+  setServo(3, 90);
+  
   servoBase_val = servoBase.read();
   servoShoulder_val = servoShoulder.read();
   servoElbow_val = servoElbow.read();
@@ -94,14 +139,41 @@ void setOledDisplay(){
   display.println("instructables");
   display.println();
   display.display();
-  delay(2000);
+  //delay(2000);
 }
 
 void loop(){
+  int detection = readDistanceSensors(); 
+  //Serial.println("Detection: " +String(detection));
+  if(controlState==SEMI_AUTONOMOUS){
+    checkIfObjectWasDetected(detection);
+  }
+  
   while(HC05.available()){
     processInput();
   }
 }// void loop()
+
+void checkIfObjectWasDetected(int detection){
+  //int targetServoPosition; 
+  if(detection==1){ // position of the right sensor
+    //targetServoPosition = 0;
+    setServoBasePosition(0);
+  }
+  else if(detection==2){ // position of the front sensor
+    //targetServoPosition = 90;
+    setServoBasePosition(90);
+  }
+  else if(detection==3){ // position of the left sensor
+    //targetServoPosition = 180;
+    setServoBasePosition(180);
+  }
+  else{
+    // not found any object
+    delay(500); 
+  }
+  //setServoBasePosition(targetServoPosition);
+}
 
 void processInput (){
   byte c = HC05.read ();
@@ -156,14 +228,36 @@ void processInput (){
   }// switch (c)
 }// void processInput ()
 
+int readDistanceSensors(){
+    int detection = 0;
+    distanceSensorRight = analogRead(distanceSensorRightPin);
+    distanceSensorFront = analogRead(distanceSensorFrontPin);
+    distanceSensorLeft = analogRead(distanceSensorLeftPin);
+    
+    Serial.println("SensorRight: "+String(distanceSensorRight)+", SensorFront: "+String(distanceSensorFront)+", SensorLeft: "+String(distanceSensorLeft));
+    
+    if(distanceSensorFront>detectionThreshold){
+      detection = 2;
+    }
+    else if(distanceSensorRight>detectionThreshold){
+      detection = 1;
+    }  
+    else if(distanceSensorLeft>detectionThreshold){
+      detection = 3;
+    }   
+    return detection;
+}// int readDistanceSensors() 
+
 void gripperControl(char mdirection, int mspeed){
-  if (mdirection == 'c'){
+  if (mdirection == 'o'){
     digitalWrite(GRIPPER_MOTOR_AIN1, LOW); 
     digitalWrite(GRIPPER_MOTOR_AIN2, HIGH); 
+    DisplayGripperStateOnOled("open");
   }
-  else if (mdirection == 'o'){
+  else if (mdirection == 'c'){
     digitalWrite(GRIPPER_MOTOR_AIN1, HIGH); 
     digitalWrite(GRIPPER_MOTOR_AIN2, LOW); 
+    DisplayGripperStateOnOled("close");
   }
   analogWrite(GRIPPER_MOTOR_PWM, mspeed);
   delay(500);
@@ -197,6 +291,35 @@ void turnLed(int state){
   }
 }
 
+void setServoBasePosition(int targetPosition){
+  if(servoBase_val==targetPosition){
+    // do nothing
+  }
+  else if(baseServoSpeedFlag==0){
+      if(servoBase_val<targetPosition){
+        for(servoBase_val; servoBase_val<targetPosition; servoBase_val +=1)// in steps of 1 degree   
+        {                                 
+          servoBase.write(servoBase_val); // tell servo to go to "currentServoPosition" 
+          delay(15);                       // waits 15ms for the servo to reach the position 
+        } 
+      }
+      
+      else if(servoBase_val>targetPosition){
+          for(servoBase_val; servoBase_val>targetPosition; servoBase_val -=1)  
+          {                                  
+            servoBase.write(servoBase_val);              
+            delay(15);                      
+          } 
+      }
+  }
+  else{
+          servoBase.write(targetPosition);      // tell servo to go to "targetPosition"
+          delay(100);
+          servoBase_val=targetPosition;
+  }
+  delay(1000);
+}// void setServoPosition(int targetPosition)
+
 void setServo(int servoNumber, int deg){
   if(servoNumber==1 && deg>=BASE_MIN && deg<=BASE_MAX){
     servoBase.write(deg);
@@ -225,6 +348,16 @@ void DisplayAngleOnOled(String armString, int agle){
     display.setCursor(20,20);
     display.print(armString);
     display.println(agle);
+    display.println();
+    display.display();
+}
+
+void DisplayGripperStateOnOled(String gripperStateString){    
+    display.clearDisplay();
+    display.setTextSize(3);
+    display.setTextColor(WHITE);
+    display.setCursor(20,20);
+    display.print(gripperStateString);
     display.println();
     display.display();
 }
